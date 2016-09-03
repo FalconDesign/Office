fc5f535aa45c75f00c46efbf9af0d9847c9e0ae8
FalconDesign <hebgax0@gmail.com>
2016-09-03 04:18:49 +0300

    es - jquery bug

--- website/app/function.js
+++ website/app/function.js
@@ -43,7 +43,7 @@ $(function () {
         team          = $('.team'),
         great         = $('.great'),
         connect       = $('.connect'),
-        windowScroll  = $(undefined).scrollTop();
+        windowScroll  = $(window).scrollTop();
 
     var windowsize = $window.width();
     if (windowsize < 678) {
@@ -75,7 +75,7 @@ $(function () {
 
   $window.on('scroll', function () {
 
-    var windowScroll  = $(undefined).scrollTop();
+    var windowScroll  = $(window).scrollTop();
     var skills        = $('.skill-container');
     var one           = $('.one');
     var two           = $('.two');
